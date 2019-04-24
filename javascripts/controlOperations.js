df9ad74814a86b9034bb7da8f6cf566c2c4267f9
irtherrien <47435618+irtherrien@users.noreply.github.com>
2019-04-24 12:09:22 -0500

    Update controlOperations.js
    Recognizes which html page has been loaded - then put in applicable functions. if any.

--- javascripts/controlOperations.js
+++ javascripts/controlOperations.js
@@ -6,9 +6,29 @@
 var i; //for for loops
 
 function executeLater() {
+   //check which page browser is on 
+   var pathname = window.location.pathname;
+   
+   switch(pathname){
+         case: "/index.html";
+               //do functions of stuff thats on the index page
+               break;
+         case: "/living.html";
+               //do functions of stuff thats on the living page
+               break;
+         case: "/visit.html";
+               //do functions of stuff thats on visit page
+               break;
+         case: "/eat.html";
+         
+               //you get the point 
+               break;
+   }
    
     
 }
+
+//some other functions go here 
 
 
 
